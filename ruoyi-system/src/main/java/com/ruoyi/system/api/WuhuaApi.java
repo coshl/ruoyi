90b1fb3901dc94ade97382d67e5b49e44f8c04ab
coshl <915929579@qq.com>
2023-04-07 22:04:02 +0800

    wuhua

--- ruoyi-system/src/main/java/com/ruoyi/system/api/WuhuaApi.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/api/WuhuaApi.java
@@ -2,6 +2,8 @@ package com.ruoyi.system.api;
 
 
 import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -22,13 +24,16 @@ public  static  String getRadar(TreeMap map){
             addParam("name", name).
             addParam("merCode", "me-1e73ec5fd48828c7ed5dd7b6e8fb8").*/
     map.put("merCode", "me-1e73ec5fd48828c7ed5dd7b6e8fb8");
+    JSONObject jsonObject = JSONUtil.parseObj(map);
+    log.info(jsonObject.toString());
     //String result = OkHttpUtils.builder().url("http://122.226.223.231:5555/ext/v2/queryRadar").
     String result = HttpRequest.post("http://122.226.223.231:5555/ext/v2/queryRadar").
             header("Content-Type", "application/json; charset=utf-8").
             header("appCode", "b809fe01f3c64c5284c06ff3c1fabe50").
-            form(map).
+            body(jsonObject.toString()).
             timeout(3000).//超时，毫秒
-            execute().body();
+            execute().
+            body();
     //System.out.println("getRadar==========="+result);
     log.info("getRadar==========="+result);
     return result;
@@ -41,12 +46,16 @@ public  static  String getRadar(TreeMap map){
         map.put("idCard", "210114199702195425");
         map.put("name", "白桂莲");
         map.put("merCode", "me-3NeuEpsHDznhz5tNWTCUkcNyM4CtEybGPq");
+        //form(map). 是表单 body(map).是json
+        JSONObject jsonObject = JSONUtil.parseObj(map);
+        System.out.println(jsonObject);
         String result = HttpRequest.post("http://122.226.223.231:5555/ext/v2/queryRadar").
                 header("Content-Type", "application/json; charset=utf-8").
                 header("appCode", "b809fe01f3c64c5284c06ff3c1fabe50").
-                form(map).
+                body(jsonObject.toString()).
                 timeout(3000).//超时，毫秒
-                execute().body();
+                execute().
+                body();
         System.out.println(result);
     }
 
