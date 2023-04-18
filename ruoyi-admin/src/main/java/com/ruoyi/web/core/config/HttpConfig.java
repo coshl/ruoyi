75086690a79296427c0af5e62f18a1dc3433d97e
coshl <915929579@qq.com>
2023-04-18 17:36:27 +0800

    https

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/HttpConfig.java
@@ -0,0 +1,28 @@
+package com.ruoyi.web.core.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Decription: 配置同时支持 HTTP 与 HTTPS 访问
+ **/
+@Configuration
+public class HttpConfig {
+    @Value("${server.custom.httpPort}")
+    private Integer httpPort;
+    
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+
+}
